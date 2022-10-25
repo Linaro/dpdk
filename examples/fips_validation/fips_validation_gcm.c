Linaro/dpdk

commit ad42b228c6e26e88b16504153a9338f9b387d2e7

examples/fips_validation: fix print for zero length payload
NIST GCM decrypt result vectors expects to have following print
for zero length payload instead of having no print.
"pt" = ""

Fixes: b09aac2d6e2b ("examples/fips_validation: add JSON to GCM test")

Signed-off-by: Archana Muniganti <marchana@marvell.com>

examples/fips_validation/fips_validation_gcm.c
@@ -327,6 +327,9 @@ parse_test_gcm_json_writeback(struct fips_val *val)
 				writeback_hex_str("", info.one_line_text, &tmp_val);
 				json_object_set_new(json_info.json_write_case, PT_JSON_STR,
 					json_string(info.one_line_text));
+			} else {
+				json_object_set_new(json_info.json_write_case, PT_JSON_STR,
+					json_string(""));
 			}
 		} else {
 			json_object_set_new(json_info.json_write_case, "testPassed", json_false());

commit d2a2e76630248985ecfde5d0bd60b7fa2f819242

examples/fips_validation: fix GMAC decryption output
AES GMAC decrypt test should output only
whether test passed or failed.

Fixes: ad42b228c6e2 ("examples/fips_validation: fix print for zero length payload")
Cc: stable@dpdk.org

Signed-off-by: Gowrishankar Muthukrishnan <gmuthukrishn@marvell.com>
Reviewed-by: Brian Dooley <brian.dooley@intel.com>

examples/fips_validation/fips_validation_gcm.c
@@ -330,11 +330,16 @@ parse_test_gcm_json_writeback(struct fips_val *val)
 				json_object_set_new(json_info.json_write_case, PT_JSON_STR,
 					json_string(info.one_line_text));
 			} else {
-				json_object_set_new(json_info.json_write_case, PT_JSON_STR,
-					json_string(""));
+				json_object_set_new(json_info.json_write_case, "testPassed",
+					json_true());
 			}
 		} else {
-			json_object_set_new(json_info.json_write_case, "testPassed", json_false());
+			if (!info.interim_info.gcm_data.is_gmac)
+				json_object_set_new(json_info.json_write_case, PT_JSON_STR,
+					json_string(""));
+			else
+				json_object_set_new(json_info.json_write_case, "testPassed",
+					json_false());
 		}
 	}
 